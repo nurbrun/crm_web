edddd0ae248f3ad9e9458614ea3ded33eaf1f288
Nurbrun <bruno.alfano.rizzo@gmail.com>
2014-10-28 15:20:48 -0400

    added new contact route and form

--- crm.rb
+++ crm.rb
@@ -2,6 +2,7 @@ require_relative 'rolodex'
 require_relative 'contact'
 require 'sinatra'
 
+$rolodex = Rolodex.new
 
 get '/' do
 	@crm_app_name = "Nurbrun's"
@@ -9,14 +10,13 @@ get '/' do
 end
 
 get '/contacts' do
-	@contacts = []
-	@contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
-  	@contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
-  	@contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
-	
 	erb :contacts
 end
 
 get '/contacts/new' do
 	erb :add_new
+end
+
+get '/contacts/new' do
+	erb :new_contact
 end
