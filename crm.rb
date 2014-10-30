c7a3f3531a3baf5a76a0a966ef47953ea85bd31a
Nurbrun <bruno.alfano.rizzo@gmail.com>
2014-10-30 13:39:33 -0400

    added a Johnny Bravo page and was able to navigate to it by manually changing the URL

--- crm.rb
+++ crm.rb
@@ -3,6 +3,7 @@ require_relative 'contact'
 require 'sinatra'
 
 $rolodex = Rolodex.new
+$rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
 
 get '/' do
 	@crm_app_name = "Nurbrun's"
@@ -23,3 +24,18 @@ post '/contacts' do
 	$rolodex.add_contact(new_contact)
 	redirect to('/contacts')
 end
+
+get '/contacts/1000' do
+  	@contact = $rolodex.find(1000)
+  	erb :show_contact
+end
+
+get '/contacts/modify' do
+	erb :modify
+end
+
+get '/contacts/remove' do
+	erb :remove
+end
+
+contact = $rolodex.find(1000)
---
+++ serverrequestlesson/index.rb
@@ -0,0 +1,63 @@
+<form id="new_form" action="/films" method="post">
+
+	<label for="film-title">Title</label>
+	<input id="film-title" type="text" name="title" placeholder="e.g. Back to the Future">
+
+
+	<textarea id="film-description" name="description" rows-"20" columns="100"></textarea>
+
+
+	<label>
+	<input type="radio" name="rating" value="1">
+	One
+	</label>
+
+	<div class="field">
+	<label>
+	<input type="checkbox" name"genre" value="Action">
+	Action
+	</label>
+	<label>
+	<input type="checkbox" name"genre" value="Adventure">
+	Adventure
+	</label>
+
+
+[type=text],
+[type=email] {
+	display
+}	
+
+<fieldset>
+<
+
+</fieldset>
+
+
+
+
+dropdownlist!!
+
+<div class="field">
+<label for="film-country">Country</label>
+<select name="country" id="film-country">
+<option
+
+
+
+
+<button type="submit">
+	<img src=" ">
+<form>
+
+
+
+
+mainrubyfile!
+
+get '/filsm' do
+	puts
+	puts params
+	GET "request: #{params}"
+end
+
